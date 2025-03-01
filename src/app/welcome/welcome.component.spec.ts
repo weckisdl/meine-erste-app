7ceec8e24bb0168603a2a18a326f5c0a9226672b
weckisdl <rw.sdl@gmx.de>
2025-03-01 16:16:16 +0100

    Initial commit für meine-erste-app

---
+++ src/app/welcome/welcome.component.spec.ts
@@ -0,0 +1,40 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WelcomeComponent } from './welcome.component';
+
+describe('WelcomeComponent', () => {
+  let component: WelcomeComponent;
+  let fixture: ComponentFixture<WelcomeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WelcomeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WelcomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have correct name, age, and hobby', () => {
+    expect(component.name).toBe('Wecki'); // Deinen Namen hier
+    expect(component.age).toBe(63); // Dein Alter hier
+    expect(component.hobby).toBe('Programmieren');
+  });
+
+  it('should display name in the template', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('p').textContent).toContain('Hallo, Wecki!');
+  });
+
+
+
+
+});
---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent implements OnInit {
+  name = 'Wecki';
+  age = 63; // Oder dein Alter
+  hobby = 'Programmieren';
+  clickCount = 0;
+ 
+  constructor() { }
+   ngOnInit(): void {
+  }
+
+  sayHello() {
+    this.clickCount++;
+    alert('Hallo, ' + this.name + '! Du bist ' + this.age + ' Jahre alt. ClickCount wurde '+this.clickCount+ ' mal gedrückt');
+
+  }
+
+
+
+
+}
