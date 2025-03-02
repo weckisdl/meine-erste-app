ee6e0685f88497f4a694480e93110368cfe624c1
weckisdl <rw.sdl@gmx.de>
2025-03-02 13:18:09 +0100

    Lektion 4: User-Eingaben mit ngModel hinzugefügt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Hallo, ich lerne Angular!';
   name: string = ''; // Variable für den Namen, initial leer
+
+
+  toBigLetter(){
+     this.name = this.name.toUpperCase();
+
+
+  }
+
+
 }
